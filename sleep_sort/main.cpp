13dc413f849cd33ba1696ab9d16b5627e83f7dd5
strahlistvan <strahlistvan0@gmail.com>
2018-02-03 23:41:42 +0100

    Sleep sort bugfix and code clear

--- sleep_sort/main.cpp
+++ sleep_sort/main.cpp
@@ -4,14 +4,18 @@
 #include <vector>
 using namespace std;
 
+const int MAX_INPUT_VALUE = 1000;
+const int INPUT_SIZE = 100;
+const int TIME_SLOT_RATE = 100;
+
 vector<int> result_list;
 
-void thread_delay(int seconds, int rate)
+void thread_delay(int seconds)
 {
-	chrono::duration<int> duration(seconds);
+	chrono::duration<int, ratio<1, TIME_SLOT_RATE> > duration(seconds);
 	this_thread::sleep_for(duration);
 
-	//cout<< " Finished after " << seconds << " seconds" <<endl;
+    //push the number to the global result list after thread delaying
 	result_list.push_back(seconds);
 }
 
@@ -24,7 +28,7 @@ vector<int> sleep_sort(vector<int>::iterator vec_begin, vector<int>::iterator ve
     result_list.clear();
     thread_list.resize(vec_size);
 
-	//Join threads to the main thread
+	//Running threads
 	auto it = vec_begin;
 	for (int i=0; i<vec_size; ++i)
 	{
@@ -46,14 +50,19 @@ int main(int arc, char ** argv)
 {
     srand(time(0));
     std::vector<int> input, output;
+    double approx_run_time = (double)(1.0/TIME_SLOT_RATE)*MAX_INPUT_VALUE;
 
-    chrono::duration<int, std::ratio<2> > eight_second(4);
+    cout << "Sleep sort is running." << endl;
+    cout << "INPUT SIZE : " << INPUT_SIZE << endl;
+    cout << "MAXIMUM INPUT ELEMENT VALUE: " << MAX_INPUT_VALUE << endl;
+    cout << "Approximate running time is " << approx_run_time << " seconds" << endl;
 
     auto start = chrono::high_resolution_clock::now();
 
-    for (int i=0; i<10; ++i)
+    //Generate random input
+    for (int i=0; i<INPUT_SIZE; ++i)
     {
-        input.push_back(rand()%10);
+        input.push_back(rand()%MAX_INPUT_VALUE+1);
     }
 
     output = sleep_sort(input.begin(), input.end());
@@ -61,12 +70,12 @@ int main(int arc, char ** argv)
 	//Print the result
 	for (int i=0; i<output.size(); ++i)
     {
-        cout << i << " output. = " << output[i] << endl;
+        cout << "output[" << i << "] = " << output[i] << endl;
     }
 
     auto end = chrono::high_resolution_clock::now();
     chrono::duration<double, milli> elapsed = end-start;
-    cout << "Waited " << elapsed.count() << " ms\n";
+    cout << "Real running time: " << elapsed.count() << " ms\n";
 
     return 0;
 }
